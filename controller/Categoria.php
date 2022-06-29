293885007d195abb1933e2bec86b545c7475c7f1
Gustavo de Freitas Caniello da Rosa <14442305650@DC01.IFS.IFSULDEMINAS.EDU.BR>
2022-06-29 16:55:46 -0300

    front-controller

--- controller/Categoria.php
+++ controller/Categoria.php
@@ -1,12 +1,31 @@
 <?php
 
-    require "../model/CategoriaModel.php";
+    require "model/CategoriaModel.php";
 
-    $categoria = new CategoriaModel();
-    $categoria->inserir("SmartTV");
-    $categoria->excluir(2);
-    $categoria->atualizar("Smartphone",3);
-    var_dump($categoria->buscarPorId(8));
-    var_dump($categoria->buscarPorTodas());
+    class Categoria{
+        function __construct()
+        {
+            $this->modelo = new CategoriaModel();
+        }
+
+        function index(){
+            include "view/template/conteudo.php";
+        }
+
+        function add(){
+            echo "mostrar form categoria";
+        }
+
+        function excluir($id){
+            echo "excluir categoria";
+        }
+    }
+
+    //$categoria = new CategoriaModel();
+    //$categoria->inserir("SmartTV");
+    //$categoria->excluir(2);
+    //$categoria->atualizar("Smartphone",3);
+    //var_dump($categoria->buscarPorId(8));
+    //var_dump($categoria->buscarPorTodas());
     
 ?>
