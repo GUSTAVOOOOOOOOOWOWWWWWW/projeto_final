84ecce86ceb87c822c29041127aa6e459bb85428
Gustavo de Freitas Caniello da Rosa <14442305650@DC01.IFS.IFSULDEMINAS.EDU.BR>
2022-07-06 13:15:48 -0300

    salvei

--- view/categoria/form.php
+++ view/categoria/form.php
@@ -1,5 +1,13 @@
 <!-- Conteúdo da página -->
 <div class="container">
     <h1 class="mt-2">Cadastro de Categoria</h1>
-     <hr>
+    <hr>
+    <form method="POST" action="<?= base_url(). "?c=categoria&m=inserir"?>">
+        <div class="mb-3">
+            <label for="categoria" class="form-label">Nome</label>
+            <input type="email" class="form-control" id="categoria" name="categoria">
+        </div>
+        <input type="hidden" name="idcategoria" value="<?= $categoria['idcategoria'] ?? '' ?>">
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
 </div>
